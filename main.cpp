cc1fab80fb5eb16cf582ba40aec120d0f2581b54
Sandro <luisgonzalezsolalinde@gmail.com>
2021-03-08 19:09:54 -0600

    Commented code // Added image select options

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/*
+Proyecto: Visión para robots
+Profesor José Luis Gordillo
+
+Equipo 5
+Luis Sandro González Solalinde A01365445
+Nathalie Vichis Lagunes A01364838
+Delia Itzel López Dueñas A00821792
+*/
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -11,14 +20,9 @@ VideoCapture camera;
 Mat currentImage;
 Mat grayImage;
 Mat binaryImage;
-vector<float> yiq(int R, int G, int B){
-    vector<float> yiq;
-    yiq.push_back((0.299*R+0.587*G+0.114*B)/255);
-    yiq.push_back((0.596*R-0.275*G-0.321*B)/255);
-    yiq.push_back((0.212*R-0.523*G+0.311*B)/255);
-    return yiq;   
-}
 
+
+//Cambia una imágen a escala de grises
 void rgbToBW(const Mat &original, Mat &gray){
     if(gray.empty())
         gray = Mat(original.rows, original.cols, original.type());
@@ -34,10 +38,41 @@ void rgbToBW(const Mat &original, Mat &gray){
     }
 }
 
+//Binariza una imágen
 void binarize(const Mat &original, Mat &bin,int thresh){
     threshold(original,bin,thresh,255,THRESH_BINARY);
 }
 
+//Obtiene los valores de YIQ de un pixel RGB
+vector<float> yiq(int R, int G, int B){
+    vector<float> yiq;
+    yiq.push_back((0.299*R+0.587*G+0.114*B)/255);
+    yiq.push_back((0.596*R-0.275*G-0.321*B)/255);
+    yiq.push_back((0.212*R-0.523*G+0.311*B)/255);
+    return yiq;   
+}
+
+//Función de reacción al click en la imagen
+void mouseClicked(int event, int x, int y, int flags, void* param){
+    Vec3b pix = currentImage.at<Vec3b>(y,x);
+    vector<float> yiq_vec = yiq((int)pix[2],(int)pix[1],(int)pix[0]);
+    switch (event)
+    {
+        case EVENT_LBUTTONDOWN:
+            printf("\033[2J");
+            printf("\033[%d;%dH", 0, 0);
+            cout << "X: " << x << " Y: "<< y <<endl;
+            cout << "R: " << (int)pix[2] << " G: " << (int)pix[1]<< " B: " << (int)pix[0]<<endl;
+            cout << "Y: " << yiq_vec[0] << " I: " << yiq_vec[1]<< " Q: " << yiq_vec[2]<<endl;
+            break;
+        case EVENT_MOUSEMOVE:
+            break;
+        case EVENT_LBUTTONUP:
+            break;
+    }
+}
+
+//Función para crear la imagen en fomrato YIQ (Work in progress)
 /*void makeYIQ(Mat &original){
     Mat YIQIBOI = original.clone();
     Vec3b pix;
@@ -52,67 +87,68 @@ void binarize(const Mat &original, Mat &bin,int thresh){
     imshow("YIQ",YIQIBOI);
 }*/
 
-void mouseClicked(int event, int x, int y, int flags, void* param){
-    Vec3b pix = currentImage.at<Vec3b>(y,x);
-    vector<float> yiq_vec = yiq((int)pix[2],(int)pix[1],(int)pix[0]);
-    switch (event)
-    {
-        case EVENT_LBUTTONDOWN:
-            printf("\033[2J");
-            printf("\033[%d;%dH", 0, 0);
-            cout << "\r\rR: " << (int)pix[2] << " G: " << (int)pix[1]<< " B: " << (int)pix[0]<<endl;
-            cout << "Y: " << yiq_vec[0] << " I: " << yiq_vec[1]<< " Q: " << yiq_vec[2]<<endl;
-            break;
-        case EVENT_MOUSEMOVE:
-            break;
-        case EVENT_LBUTTONUP:
-            break;
-    }
-}
-
-
 int main(int argc, char *argv[]){
     camera.open(0);
-    namedWindow("Image");
-    setMouseCallback("Image", mouseClicked);
+    namedWindow("Camera");
+    setMouseCallback("Camera", mouseClicked);
     int thresh = 0;
-    bool clicked = false, run = true, sel = true;
+    char sel = 'a';
+    bool clicked = false, run = true;
     while (run)
-	{	
-		if(!clicked)
-			camera >> currentImage;
-		if (currentImage.data) 
-		{
-            rgbToBW(currentImage,grayImage);
-            binarize(grayImage,binaryImage,thresh);
-            if(sel)
-                imshow("Image", currentImage);
-            else
-                imshow("Gray",grayImage);
-            //imshow("Binary",binaryImage);
-            switch(waitKey(3)){
-                case 't':
-                cout<<"Threshold: ";
-                cin>>thresh;
+    {   
+        if(!clicked)
+            camera >> currentImage;
+        if (currentImage.data) 
+        {
+            switch(sel){
+                case 'a':
+                    imshow("Camera", currentImage);
+                    break;
+                case 'b':
+                    rgbToBW(currentImage,grayImage);
+                    imshow("Grayscale",grayImage);
+                    break;
+                case 'c':
+                    rgbToBW(currentImage,grayImage);
+                    binarize(grayImage,binaryImage,thresh);
+                    imshow("Binarized",binaryImage);
                 break;
-                case 'p':
-                clicked = !clicked;
-                break;
-                case 's':
-                destroyAllWindows();
-                sel = !sel;
-                break;
-                case 'x':
-                run = false;
+                default:
+                    printf("\033[2J");
+                    printf("\033[%d;%dH", 0, 0);
+                    cout<<"Incorrect image, please try again.";
+                    run = false;
                 break;
             }
+
+            switch(waitKey(3)){
+                case 'p':
+                    clicked = !clicked;
+                    break;
+                case 's':
+                    destroyAllWindows();
+                    printf("\033[2J");
+                    printf("\033[%d;%dH", 0, 0);
+                    cout<<"Select an image:\na)Camera\nb)Grayscale\nc)Binarized\n";
+                    cin>>sel;
+                    if(sel == 'c'){
+                        printf("\033[2J");
+                        printf("\033[%d;%dH", 0, 0);
+                        cout<<"Threshold: ";
+                        cin>>thresh;
+                    }
+                    break;
+                case 'x':
+                    run = false;
+                    break;
+            }
               
-		}
-		else
-		{
-			cout << "No image data.. " << endl;
-		}
-	}
+        }
+        else
+        {
+            cout << "No image data.. " << endl;
+        }
+    }
 }
 
 
