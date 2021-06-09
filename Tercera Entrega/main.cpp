f89e1aae2f1d8473698c3ae74ad2d6844df50ef9
Cristian Ramirez <cris.aurelio99@gmail.com>
2021-06-09 02:40:30 -0500

    Mira added but still not implemented

--- Tercera Entrega/main.cpp
+++ Tercera Entrega/main.cpp
@@ -4,7 +4,7 @@ Profesor José Luis Gordillo
 Equipo 5
 Luis Sandro González Solalinde A01365445
 Nathalie Vichis Lagunes A01364838
-Cristian
+Cristian Aurelio Ramírez Anzaldo A01066337
 Carlos
 */
 #include <opencv2/imgproc/imgproc.hpp>
@@ -42,6 +42,418 @@ enum lastStep{
     UP,
     DOWN
 }last;
+
+/*
+    	  __^__                                      __^__
+         ( ___ )------------------------------------( ___ )
+          | / |                                      | \ |
+          | / |        Variables para la mira        | \ |
+          |___|                                      |___|
+         (_____)------------------------------------(_____)
+*/
+
+vector<int> seedX,seedY,m00,m01,m20,m02,m11,m10,mu20,mu02,mu11;
+vector <float> n20,n02,n11,fi1,fi2,fis1,fis2;
+Vec3b pinto;
+Mat currentImageMira, segmentedMira;
+const float maxApplefi1   = .13,
+        maxPearfi1    = .17,
+        maxBanannafi1 = 0.24,
+        maxCarrotfi1  = .4,
+        minApplefi1   = .08,
+        minPearfi1    = .12,
+        minBanannafi1 = 0.18,
+        minCarrotfi1  = .25;
+
+
+/*
+                               ......
+                            .:||||||||:.
+                           /            \
+                          (   o      o   )
+                --@@@@----------:  :----------@@@@--
+                                MIRA
+                               FRUTAS
+ */
+
+
+
+/*
+    	  __^__                                      __^__
+         ( ___ )------------------------------------( ___ )
+          | / |                                      | \ |
+          | / |                inBounds              | \ |
+          |___|                                      |___|
+         (_____)------------------------------------(_____)
+ * Funcion para detectar que las phi estén dentro de los límites
+ * reconocidos
+ * Parametros:
+ *      val: valor actual
+ *      min, max: valores máximos y mínimos de la phi
+ *
+ */
+bool inBounds(float val, float min, float max){
+    if(val<=max && val>=min)
+        return true;
+    return false;
+}
+
+/*
+    	  __^__                                      __^__
+         ( ___ )------------------------------------( ___ )
+          | / |                                      | \ |
+          | / |                IDENTIFY              | \ |
+          |___|                                      |___|
+         (_____)------------------------------------(_____)
+
+ * Funcion para hacer el reconocimiento de la figura
+ *
+ * Parametros:
+ *      fi1, f2: valores fi de la figura mostrada en camara
+ *
+*/
+bool identify(float fi1, float fi2){
+
+    // Bool to identify large figure
+    bool bLargeFigure;
+
+    // revisa si la figura es una banana o zanahoria
+    if(inBounds(fi1,minBanannafi1,maxBanannafi1) ){
+        cout<<"Plátano reconocido"<<endl;
+        big=1;
+        bLargeFigure = true;
+    }
+    if(inBounds(fi1,minCarrotfi1,maxCarrotfi1) ){
+        cout<<"Zanahoria reconocida"<<endl;
+        big=2;
+        bLargeFigure = true;
+    }
+
+    return bLargeFigure;
+}
+
+
+/*
+    	  __^__                                      __^__
+         ( ___ )------------------------------------( ___ )
+          | / |                                      | \ |
+          | / |                 PAINT                | \ |
+          |___|                                      |___|
+         (_____)------------------------------------(_____)
+ * Funcion para pintar los segmentos
+ *
+ * Parametros:
+ *      original: matriz imagen original
+ *      segImg: Imagen segmentada
+ *      x, y: coordenadas del punto a cambiar de  color
+ *
+ */
+void paintMira(const Mat &original, Mat &segImg,int x, int y){
+    if(debug)cout<<"Empieza paint"<<endl;
+
+    pinto[0] = 254 *(N-2)*(N-3);
+    pinto[1] = 200 *(N-1)*(N-3);
+    pinto[2] = 200 *(N-2)*(N-1);
+
+    segImg.at<Vec3b>(y,x) = pinto;
+    m00[N-1]= m00[N-1]+1;
+    m01[N-1]= m01[N-1]+ y;
+    m10[N-1]= m10[N-1]+ x;
+
+    m02[N-1]= m02[N-1] + pow(y,2);
+    m20[N-1]= m20[N-1] + pow(x,2);
+    m11[N-1]= m11[N-1] + x*y;
+
+
+    Vec3b fondo;
+    fondo[0]=0;
+    fondo[1]=0;
+    fondo[2]=0;
+
+    if(segImg.at<Vec3b>(y,x+1)!= fondo && segImg.at<Vec3b>(y,x+1)!=pinto){
+        seedX.push_back(x+1);
+        seedY.push_back(y);
+    }
+
+    if(segImg.at<Vec3b>(y,x-1)!= fondo && segImg.at<Vec3b>(y,x-1)!=pinto){
+        seedX.push_back(x-1);
+        seedY.push_back(y);
+    }
+
+    if(segImg.at<Vec3b>(y+1,x)!= fondo && segImg.at<Vec3b>(y+1,x)!=pinto){
+        seedX.push_back(x);
+        seedY.push_back(y+1);
+    }
+
+    if(segImg.at<Vec3b>(y-1,x)!= fondo && segImg.at<Vec3b>(y-1,x)!=pinto){
+        seedX.push_back(x);
+        seedY.push_back(y-1);
+    }
+    if(debug)cout<<"Termina paint"<<endl;
+}
+
+
+
+/*
+    	  __^__                                      __^__
+         ( ___ )------------------------------------( ___ )
+          | / |                                      | \ |
+          | / |                SEGMENT               | \ |
+          |___|                                      |___|
+         (_____)------------------------------------(_____)
+ * Funcion con datos de semilla y segmento
+ *
+ * Parametros:
+ *      original: matriz imagen original
+ *      segImg: Imagen segmentada
+ *
+ */
+
+void segmentMira(const Mat &original, Mat &segImg){
+    if(debug)cout<<"Empieza segment"<<endl;
+    int x , y;
+
+    while(!seedX.empty()){
+        x = seedX.back();
+        y = seedY.back();
+        seedX.pop_back();
+        seedY.pop_back();
+        paintMira(original,segImg,x,y);
+    }
+    if(debug)cout<<"Termina segment"<<endl;
+}
+
+
+/*
+    	  __^__                                      __^__
+         ( ___ )------------------------------------( ___ )
+          | / |                                      | \ |
+          | / |                  SEED                | \ |
+          |___|                                      |___|
+         (_____)------------------------------------(_____)
+ * Funcion para encontrar la semilla a un segmento
+ * Parametros:
+ *      original: Matriz imagen original
+ *      height, width: alto y ancho de la figura
+ *
+ */
+void seedMira(const Mat &original, int height, int width, int offSet) {
+
+    // Boolean to stay in loop until a seed is found
+    bool bSeed = false;
+    int rand_X;
+    int rand_Y;
+
+    Vec3b fondo;
+    fondo[0]=0;
+    fondo[1]=0;
+    fondo[2]=0;
+
+    while (!bSeed) {
+
+        // Getting rand number in image size range
+        rand_X = rand() % width + offSet; // Offset it is the initial value for random
+        rand_Y = rand() % height;
+
+        if(rand_Y<original.rows-2 && rand_X<original.cols-2 && rand_X>-1 && rand_Y>-1){
+            if (original.at<Vec3b>(rand_Y, rand_X) != fondo){
+                bSeed = true;
+                seedX.push_back(rand_X);
+                seedY.push_back(rand_Y);
+            }
+        }
+    }
+}
+
+
+
+/*
+    	  __^__                                      __^__
+         ( ___ )------------------------------------( ___ )
+          | / |                                      | \ |
+          | / |               buscaMira              | \ |
+          |___|                                      |___|
+         (_____)------------------------------------(_____)
+ * Funcion para buscar los segmentos
+ * Parametros:
+ *      none
+ *
+ */
+
+void buscaMira(){
+    if(debug)cout<<"Empieza busca"<<endl;
+    // Getting size of image
+
+    Size s = segmentedMira.size();
+    if(debug)cout<<"Se obtiene size"<<endl;
+    int height = s.height;
+    int width = s.width;
+
+    // Variables para graficar angulo
+    double arrowXFigure, arrowYFigure;
+
+    //inicializar momentos
+
+    m00.push_back(0);
+    m01.push_back(0);
+    m10.push_back(0);
+    m20.push_back(0);
+    m02.push_back(0);
+    m11.push_back(0);
+    // doble o falla el segundo centroide
+
+    m00.push_back(0);
+    m01.push_back(0);
+    m10.push_back(0);
+    m20.push_back(0);
+    m02.push_back(0);
+    m11.push_back(0);
+
+
+    if(debug)cout<<"Empieza seed"<<endl;
+    seedMira(segmentedMira, height, width/2, 0);
+    if(debug)cout<<"Termina seed"<<endl;
+
+    segmentMira(currentImageMira,segmentedMira);
+    if(debug)cout <<"Termina segment"<<endl;
+
+    if(debug)cout << "Area 1 = " << m00[0] <<endl;
+
+    //centroide figura 1
+    cx1 = (m10[0]/(m00[0]+ 1e-5)); //add 1e-5 to avoid division by zero
+    cy1 = (m01[0]/(m00[0]+ 1e-5)); // float ?
+    if(debug)cout << "suma X " << m10[0] <<endl;
+    if(debug)cout << "suma Y " << m01[0] <<endl;
+    if(debug)cout << "CX " << cx1 <<endl;
+    if(debug)cout << "CY " << cy1 <<endl;
+    circle (segmentedMira,Point(cx1,cy1),4,(255,0,0),-1);
+
+
+    // momentos segundo orden
+    mu20.push_back(m20[0] - (cx1*m10[0]));
+    mu02.push_back(m02[0] - (cy1*m01[0])) ;
+    mu11.push_back(m11[0] - (cy1*m10[0])) ;
+
+
+    //momentos normalizados
+    n20.push_back((float) (mu20[0]/pow(m00[0],2)));
+    n02.push_back((float) (mu02[0]/pow(m00[0],2)));
+    n11.push_back((float) (mu11[0]/pow(m00[0],2)));
+
+    // fi 1 y fi 2
+    fi1.push_back(n20[0]+n02[0]+ 1e-5);
+    fi2.push_back(pow((n20[0]-n02[0]),2)+4*pow(n11[0],2)+ 1e-5);
+    if(debug)cout << "Fi1  " << fi1[0] <<endl;
+    if(debug)cout << "Fi2  " << fi2[0] <<endl;
+    bool isLarge = identify(fi1[0],fi2[0]);
+    // recolentacndo valores de fi para ENTRENAMIENTO, solo una figura
+
+    fis1.push_back(fi1[0]);
+    fis2.push_back(fi2[0]);
+    if(debug)cout<<" , "<< fi1 [0]<< " , " << fi2 [0]  << endl;
+
+    // ************************************** /Angulo de la figura 1\ **************************************
+    //if (isLarge) {
+    double theta = 0.5 * atan2((2*mu11.back()), mu20.back() - mu02.back());
+    if(debug)cout << "Angle is " << theta << endl;
+    double arrowHeadX = 100.0; // width of the figure. SET LATER WITH REAL VALUES ----------------------------------
+    double arrowHeadY = tan(theta) * arrowHeadX;
+
+    double arrowTailX = 100.0; // width of the figure. SET LATER WITH REAL VALUES ----------------------------------
+    double arrowTailY = tan(theta) * arrowTailX;
+
+    // Drawing line
+    line(segmentedMira, Point(cx1-arrowTailX, cy1 - arrowTailY), Point(cx1+arrowHeadX,
+                                                                   cy1+arrowHeadY), (255, 100, 100), 3);
+    // Drawing line
+    line(segmentedMira, Point(cx1+arrowTailX, cy1 - arrowTailY), Point(cx1-arrowHeadX,
+                                                                   cy1+arrowHeadY), (255, 100, 100), 3);
+    //}
+
+
+    m00.clear(); m10.clear(); m01.clear(); m20.clear(); m02.clear();
+    m11.clear(); mu02.clear(); mu20.clear(); mu11.clear();
+    n20.clear(); n02.clear();
+    fi1.clear(); fi2.clear();
+
+    if(debug)cout<<"Termina busca"<<endl;
+}
+
+
+/*
+    	  __^__                                      __^__
+         ( ___ )------------------------------------( ___ )
+          | / |                                      | \ |
+          | / |           buscaWrappedMira           | \ |
+          |___|                                      |___|
+         (_____)------------------------------------(_____)
+ * Funcion para buscar objeto en threads
+ * Parametros:
+ *      none
+ *
+ */
+int buscaWrappedMira()
+{
+    std::mutex m;
+    std::condition_variable cond;
+    int retValue;
+
+    std::thread t([&cond, &retValue]()
+                  {
+                      buscaMira();
+                      cond.notify_one();
+                  });
+
+    t.detach();
+
+    {
+        std::unique_lock<std::mutex> l(m);
+        if(cond.wait_for(l, 1s) == std::cv_status::timeout)
+            throw std::runtime_error("Timeout");
+    }
+
+    return retValue;
+}
+
+
+
+/*
+    	  __^__                                      __^__
+         ( ___ )------------------------------------( ___ )
+          | / |                                      | \ |
+          | / |                SEPARAR               | \ |
+          |___|                                      |___|
+         (_____)------------------------------------(_____)
+ * Funcion para separar imagen de la que se utilizará
+ * Parametros:
+ *      original: Matriz imagen original
+ *      editRGB: Matriz para trabajar
+ *
+ */
+void separarMira(const Mat &original, Mat &editRGB){
+    Mat maskRGB;
+    //Reset image
+    editRGB = Mat(0, 0, 0, Scalar( 0,0,0));
+    inRange(original, Scalar(0,0,125), Scalar(90,90,255),maskRGB);
+    original.copyTo(editRGB,maskRGB);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*void routeFinder(Mat &image, int startX, int startY, int finishX, int finishY, int stepSize){
     cout<<"Empieza route"<<endl;
@@ -100,7 +512,7 @@ enum lastStep{
                 }
 
     		}
-    		
+
     	}else{
     		if(startY-currentY >0){
     			if(image.at<Vec3b>(currentY+stepSize,currentX)[0] > 10){
@@ -162,6 +574,25 @@ enum lastStep{
     cout<<"Termina route"<<endl;
 }*/
 
+
+
+
+
+/*
+                                __
+                             .'  '.
+                         _.-'/  |  \
+            ,        _.-"  ,|  /  0 `-.
+            |\    .-"       `--""-.__.'=====================-,
+            \ '-'`        .___.--._)=========================|
+             \            .'      |                          |
+              |     /,_.-'        |      PLANIFICADOR        |
+            _/   _.'(             |           DE             |
+           /  ,-' \  \            |         CAMINOS          |
+           \  \    `-'            |                          |
+            `-'                   '--------------------------'
+
+ */
 void paint(Mat &img, int x, int y){
     if(y>0)img.at<Vec3b>(x-1,x) = {0,0,255};
     if(y<img.rows-1)img.at<Vec3b>(y+1,x) = {0,0,255};
@@ -412,7 +843,43 @@ void camino(){
 }
 
 
+/*
+MAIN MAIN MAIN MAIN                    ________
+MAIN MAIN MAIN MAIN                /--/        \
+              MAIN                |   \______   |
+          MAIN                     \ - ---^^- / /
+       MAIN                                  ||/
+          MAIN                               |||
+              MAIN                         .:'':.
+MAIN MAIN MAIN MAIN                  /^/^^\/     \
+MAIN MAIN MAIN MAIN              0___O_\O_/       |
+                                 |               /              O
+MAIN                             |       .._    /              //
+    MAIN                          \ ____/   |  |              //
+         MAIN                               |  |             //
+    MAIN      MAIN                   ^^^^^  |  |            ||
+        MAIN                      ^^^^^^^^^^|  \         __ /|
+    MAIN                         ^^^^^^   ^^|   \       /     \
+MAIN                             ^^^^^    __|    \____/        |
+                                   ^^^    \    /               |
+MAIN           MAIN                         \-/          (_     \
+MAIN MAIN MAIN MAIN                          |  |\__________\   |
+MAIN MAIN MAIN MAIN                         /|  |          \ \  |
+MAIN           MAIN                 _______| |  |     ______\ \  \
+                                   /    ____/   |    /    ____/   \
+MAIN MAIN MAIN MAIN                \(_ /         \   \(_ /        |
+MAIN MAIN MAIN MAIN                    \_(____.../       \_(_____/
+            MAIN
+       MAIN
+  MAIN                                             ( ( (
+MAIN MAIN MAIN MAIN                                 ) ) )
+MAIN MAIN MAIN MAIN                                ( ( (
+                                                 '. ___ .'
+                                                '  (> <) '
+                                        --------ooO-(_)-Ooo----------
+                                                    MAIN
 
+ */
 
 int main(int argc, char *argv[]){
     camera.open(0);
@@ -423,7 +890,7 @@ int main(int argc, char *argv[]){
     while (run)
     {
         if(!clicked){
-        //camera >> currentImage;
+            camera >> currentImageMira;
         }
         
         if (currentImage.data)
@@ -442,6 +909,10 @@ int main(int argc, char *argv[]){
                     rgbToBW(currentImage,grayImage);
                     imshow("Grayscale",grayImage);
                     break;
+
+                    /*
+                     ███▓▒░░.OPCION DE CAMINOS AUTOMATICA SIN MIRA.░░▒▓███
+                     */
                 case 'c':
                     if(debug)cout << "c";
                     
@@ -461,6 +932,15 @@ int main(int argc, char *argv[]){
                     if(animationImage.data)
                         imshow("Animated",animationImage);
                     break;
+                    /*
+                     *     oooO
+                          (....)     Oooo
+                           ...(     (....)
+                            ._)      )../
+                                     (_/
+                        OPCION PARA PLANIFICADOR
+                        DE CAMINOS CON MIRA
+                     */
                 case 'd':
                     if(debug)cout << "d";
                     
@@ -480,6 +960,27 @@ int main(int argc, char *argv[]){
                     if(animationImage.data)
                         imshow("Animated",animationImage);
                     break;
+
+                    /*
+                               ____
+                             _(____)_
+                      ___ooO_(_o__o_)_Ooo___
+                         OPCION PARA MIRA
+                     */
+                case 'f':
+                    if(debug)cout << "f";
+                    separarMira(currentImageMira, segmentedMira);
+                    namedWindow("OriginalMira");
+                    try{
+                        buscaWrappedMira();
+                    }
+                    catch(runtime_error& e){
+                        cout<<"Timed out, trying again."<<endl;
+                    }
+                    imshow("OriginalMira",currentImageMira);
+                    imshow("SegmentedMira",segmentedMira);
+                    break;
+
                 default:
                     cout << "default";
                     printf("\033[2J");
@@ -501,7 +1002,7 @@ int main(int argc, char *argv[]){
                     cin>>sel;
                     break;
                 case 'r':
-                    
+                    // 315x362 image size
                     if (pxy){
                     pozo();
                    	camino();
